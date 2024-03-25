80f9c128fd0b5e85b9bc708b01948ad4cae61ebc
Davihhhh <101269407+Davihhhh@users.noreply.github.com>
2024-03-25 10:51:10 +0100

    Update Form1.Designer.cs

--- Client/Client/Form1.Designer.cs
+++ Client/Client/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace ClientCB
+namespace ClientCB
 {
     partial class Form1
     {
@@ -40,6 +40,7 @@
             this.timerPartita = new System.Windows.Forms.Timer(this.components);
             this.labelPunteggio = new System.Windows.Forms.Label();
             this.buttonInizia = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
             this.SuspendLayout();
             // 
             // buttonGioca
@@ -153,11 +154,22 @@
             this.buttonInizia.Visible = false;
             this.buttonInizia.Click += new System.EventHandler(this.buttonInizia_Click);
             // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(15, 96);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(143, 326);
+            this.listView1.TabIndex = 17;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.Visible = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1306, 644);
+            this.Controls.Add(this.listView1);
             this.Controls.Add(this.buttonInizia);
             this.Controls.Add(this.labelPunteggio);
             this.Controls.Add(this.labelNome);
@@ -188,6 +200,7 @@
         private System.Windows.Forms.Timer timerPartita;
         private System.Windows.Forms.Label labelPunteggio;
         private System.Windows.Forms.Button buttonInizia;
+        private System.Windows.Forms.ListView listView1;
     }
 }
 
